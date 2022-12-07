e1f359ed2b2bf93549e23d4d2104fe60e19d2ca2
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-07 20:52:36 +0200

    init

---
+++ src/main/java/com/mycompany/firstspringeurekaserverdiscovery/FirstSpringEurekaServerDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany.firstspringeurekaserverdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+public class FirstSpringEurekaServerDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringEurekaServerDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/firstspringeurekaserverdiscovery/FirstSpringEurekaServerDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.firstspringeurekaserverdiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringEurekaServerDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
